33819f78be860fd3c46903b9833e79a9c11b4b58
Sergio <sergio.rubio@gamesys.co.uk>
2018-07-10 16:31:55 +0100

    create project

---
+++ client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	pb "golang-bcrypt-service/golangbcryptservice"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func generateBCrypt(client pb.BCryptClient, password *pb.PasswordRequest) {
+	feature, err := client.PostBCrypt(context.Background(), password)
+	if err != nil {
+		log.Fatalf("%v.GetFeatures(_) = _, %v: ", client, err)
+	}
+	log.Println(feature)
+}
+
+func main() {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+
+	conn, err := grpc.Dial("localhost:6565", opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewBCryptClient(conn)
+
+	generateBCrypt(client, &pb.PasswordRequest{Password: "password"})
+}
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+
+	pb "golang-bcrypt-service/golangbcryptservice"
+
+	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":6565"
+)
+
+type golangBcryptServer struct{}
+
+func (s *golangBcryptServer) PostBCrypt(ctx context.Context, password *pb.PasswordRequest) (*pb.PasswordHashedResponse, error) {
+	hash := hashPassword(password.GetPassword())
+	fmt.Printf("Hashed value: %v\n", string(hash))
+	return &pb.PasswordHashedResponse{Hash: hash}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Printf("Listening on port: %s", port)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterBCryptServer(grpcServer, &golangBcryptServer{})
+	grpcServer.Serve(lis)
+}
+
+func hashPassword(password string) string {
+	fmt.Printf("Value: %v\n", password)
+	rounds, _ := strconv.Atoi(os.Getenv("ROUNDS"))
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), rounds)
+	return string(hash)
+}
